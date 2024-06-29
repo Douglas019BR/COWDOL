847c831d9f2776528af2b13a966828c0ae818cda
DOUG <douglassermarini@gmail.com>
2024-06-29 11:09:15 -0300

    first commit

---
+++ get_dividends.py
@@ -0,0 +1,17 @@
+import yfinance as yf
+import pandas as pd
+
+def get_dividends(symbol, start_date):
+    stock = yf.Ticker(symbol)
+    dividends = stock.dividends
+    dividends = dividends[dividends.index >= start_date]
+    return dividends
+
+if __name__ == "__main__":
+    symbol = 'TAEE11.SA' 
+    start_date = '2023-01-01'
+    
+    dividends = get_dividends(symbol, start_date)
+    print(dividends)
+    total_dividends = dividends.sum()
+    print(f"Total de dividendos recebidos desde {start_date}: R$ {total_dividends:.2f}")
