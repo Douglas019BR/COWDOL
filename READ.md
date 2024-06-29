58a5900c536aec3503441dd9a90d553b8f66a4c0
DOUG <douglassermarini@gmail.com>
2024-06-29 17:32:49 -0300

    pyscript

---
+++ READ.md
@@ -0,0 +1,43 @@
+# Stock Portfolio Monitoring Tool
+
+
+[![GitHub Pages](https://img.shields.io/badge/GitHub-Pages-blue?style=flat-square)](https://stephensonsn.github.io/engenharia_software_tools)
+[![Python](https://img.shields.io/badge/Python-3-blue?style=flat-square)](https://www.python.org/)
+[![HTML](https://img.shields.io/badge/HTML-5-orange?style=flat-square)](https://developer.mozilla.org/en-US/docs/Web/HTML)
+[![CSS](https://img.shields.io/badge/CSS-3-blue?style=flat-square)](https://developer.mozilla.org/en-US/docs/Web/CSS)
+
+## How to use
+Acesse a [GitHub Pages](...) para utilizar o projeto.
+
+## Description
+A tool to monitor stock portfolios on B3 (Brazilian Stock Exchange). This tool allows users to track their investments, view real-time data, calculate dividends received, and analyze their portfolio performance over time.
+
+## Features
+
+- Fetches current stock prices from Yahoo Finance
+- Calculates dividends received since the purchase date
+- Computes the difference between current stock value and purchase value
+- Provides a summary of profits and losses for each stock
+
+## Requirements
+
+- [Python](https://www.python.org/)
+- [yfinance](https://pypi.org/project/yfinance/)
+- [pandas](https://pypi.org/project/pandas/)
+
+
+### Install the required libraries:
+```
+pip install -r requirements.txt
+```
+
+### Run the script:
+```
+python process.py
+```
+
+### Contributing
+Contributions are welcome! Please feel free to submit a Pull Request.
+
+### Contact
+For any inquiries, please contact [douglassermarini@gmail.com].
---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import csv
+
+ALPHA_VANTAGE_API_KEY = 'I5BDJ29HDPFX7N0W'
+
+def get_dividends(symbol, start_date):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if 'Our standard API rate limit is 25 requests per day.' in data:
+            raise Exception("API rate limit exceeded")
+        monthly_adjusted_data = data.get('Monthly Adjusted Time Series', {})
+        dividends ={}
+        for date, values in monthly_adjusted_data.items():
+            if date >= start_date:
+                dividend_amount = float(values.get('7. dividend amount', '0.0'))
+                dividends[date] = dividend_amount
+                
+        return dividends
+    else:
+        return {}
+        
+
+def get_current_price(symbol):
+    url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if 'Our standard API rate limit is 25 requests per day.' in data:
+            raise Exception("API rate limit exceeded")
+        current_price = float(data['Global Quote']['05. price'])
+        return current_price
+    else:
+        return None
+
+def process_portfolio(filename):
+    results = []
+
+    with open(filename, newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+
+        for row in reader:
+            symbol = row["SYMBOL"]
+            amount = int(row["AMOUNT"])
+            purchase_date = row["PURCHASE_DATE"]
+            purchase_value = float(row["PURCHASE_VALUE"])
+
+            
+            dividends = get_dividends(symbol, purchase_date)
+            current_price = get_current_price(symbol)
+            total_dividends = sum(dividends.values()) * amount if dividends else 0
+
+            current_value = current_price * amount
+            purchase_total = purchase_value * amount
+            value_difference = current_value - purchase_total
+            balance = value_difference + total_dividends
+
+            if balance > 0:
+                status = "Profit"
+            elif balance < 0:
+                status = "Loss"
+            else:
+                status = "No change"
+
+            message = (
+                f"Stock: {symbol}\nQuantity: {amount}\nPurchase Price per Share: ${purchase_value:.2f}\n"
+                f"Current Price per Share: ${current_price:.2f}\n"
+                f"Dividends Received: ${total_dividends:.2f}\n"
+                f"Difference (Current Price - Purchase Price): ${value_difference:.2f}\n"
+                f"Balance (Difference + Dividends): ${balance:.2f}\n"
+                f"Status: {status}\n"
+            )
+
+            results.append(message)
+
+    return results
+
+if __name__ == "__main__":
+    import pyscript
+
+    results = process_portfolio("input.csv")
+    for result in results:
+        print(result)
