143dea3491bea9d6304f69af60e66edc07fa6ab9
DOUG <douglassermarini@gmail.com>
2024-10-01 21:05:13 -0300

    help apolo with tests

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,11 @@
 from yfinance_functions import process_symbol
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from ..server import app
 
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "http://localhost*",
 ]
 
 app.add_middleware(
@@ -19,36 +17,16 @@ app.add_middleware(
 )
 
 
-@app.get("/hello-world")
-async def root():
-    return {"message": "Hello World"}
-
-
 @app.post("/profit-calculate")
 async def profit_calculate(request: dict):
     print(request)
     results = []
-    for row in request['data']:
+    # import ipdb;ipdb.set_trace()
+    for row in request["data"]:
         symbol_result = process_symbol(
-            row['SYMBOL'], row['AMOUNT'], row['PURCHASE_DATE'], row['PURCHASE_VALUE'])
-        symbol_result['symbol'] = row['SYMBOL']
-        symbol_result['amount'] = row['AMOUNT']
+            row["SYMBOL"], row["AMOUNT"], row["PURCHASE_DATE"], row["PURCHASE_VALUE"]
+        )
+        symbol_result["symbol"] = row["SYMBOL"]
+        symbol_result["amount"] = row["AMOUNT"]
         results.append(symbol_result)
     return results
-
-# {
-    #     "data": [
-    #         {
-    #             "SYMBOL": "BBSE3.sa",
-    #             "AMOUNT": 11,
-    #             "PURCHASE_DATE": "01/10/2023",
-    #             "PURCHASE_VALUE": 31.46,
-    #         }
-    #         {
-    #             "SYMBOL": "NIKE34.sa",
-    #             "AMOUNT": 7,
-    #             "PURCHASE_DATE": "09/10/2023",
-    #             "PURCHASE_VALUE": 47.04,
-    #         }
-    #     ]
-    # }
--- backend/tests/test_profit_calculate.py
+++ backend/tests/test_profit_calculate.py
@@ -1,14 +1,37 @@
+from unittest.mock import call
+import pytest
 import sys
 import os
 from fastapi.testclient import TestClient
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from server import app 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from server import app
 
 client = TestClient(app)
 
-def test_profit_calculate():
+
+@pytest.fixture
+def mocked_process_symbol(mocker):
+    return mocker.patch(
+        "server.process_symbol",
+        side_effect=[{
+            "current_value": 2,
+            "purchase_total": 2,
+            "value_difference": 2,
+            "balance": 2,
+            "total_dividends": 2,
+        },{
+            "current_value": 1,
+            "purchase_total": 1,
+            "value_difference": 1,
+            "balance": 1,
+            "total_dividends": 1,
+        }]
+    )
+
+
+def test_profit_calculate(mocked_process_symbol):
 
     request_data = {
         "data": [
@@ -23,26 +46,22 @@ def test_profit_calculate():
                 "AMOUNT": 7,
                 "PURCHASE_DATE": "09/10/2023",
                 "PURCHASE_VALUE": 47.04,
-            }
+            },
         ]
     }
 
-    
     response = client.post("/profit-calculate", json=request_data)
-    
-
+    assert mocked_process_symbol.call_args_list == [call('BBSE3.sa', 11, '01/10/2023', 31.46),call('NIKE34.sa', 7, '09/10/2023', 47.04)]
     assert response.status_code == 200
-    
 
-    # Para verificar se a resposta contém os resultados esperados.
     response_data = response.json()
-    
-    
-    assert len(response_data) == 2  
-    assert response_data[0]['symbol'] == "BBSE3.sa"
-    assert response_data[1]['symbol'] == "NIKE34.sa"  
+
+    assert len(response_data) == 2
+    assert response_data[0]["symbol"] == "BBSE3.sa"
+    assert response_data[1]["symbol"] == "NIKE34.sa"
 
 
 if __name__ == "__main__":
     import pytest
+
     pytest.main()
